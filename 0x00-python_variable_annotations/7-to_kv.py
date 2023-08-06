ca3354a9a50439607c078ec3bddf2a813369d48a
Wmugacha <wilfredmugacha@gmail.com>
2023-08-06 09:37:46 +0000

    return a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that returns a tuple
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """Function that returns a tuple"""
+
+    return (k, v ** 2)

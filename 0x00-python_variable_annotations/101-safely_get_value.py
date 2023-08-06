f307f163ede17e9eba93309dbe4ab17ccf62bd19
Wmugacha <wilfredmugacha@gmail.com>
2023-08-06 17:42:36 +0000

    TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Add annotations
+"""
+from typing import Any, Mapping, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping,
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
+    """Add annotations"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default

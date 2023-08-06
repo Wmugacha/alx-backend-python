a96c7baa6998649042b2e2973c94b80a37588816
Wmugacha <wilfredmugacha@gmail.com>
2023-08-06 07:19:33 +0000

    concat string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module of a function to concatenate a string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function to concatenate a string"""
+
+    return str1 + str2

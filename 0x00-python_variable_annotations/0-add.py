303addd35270e208f142a59e359756c57658f0d6
Wmugacha <wilfredmugacha@gmail.com>
2023-08-06 06:47:50 +0000

    add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module of an add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Function that returns the sum
+    of two float variables"""
+
+    return a + b

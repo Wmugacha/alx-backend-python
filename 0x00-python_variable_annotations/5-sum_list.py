58a80f024ed6fc0656bb67338a62c1f65dff0fbc
Wmugacha <wilfredmugacha@gmail.com>
2023-08-06 08:10:30 +0000

    sum of a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that returns the sum of a list
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Function that returns the sum of floats in a list"""
+
+    return sum(input_list)

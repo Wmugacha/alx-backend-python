011a954314a078aa8ffce8bd1d548ada2580be7d
Wmugacha <wilfredmugacha@gmail.com>
2023-08-06 09:39:45 +0000

    return a tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ Module that returns a tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Function that returns a tuple"""
 
     return (k, v ** 2)

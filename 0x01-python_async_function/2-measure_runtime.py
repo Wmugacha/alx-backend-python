359831f8b6c57ee16f489dd647ea7cc04381c59e
Wmugacha <wilfredmugacha@gmail.com>
2023-08-07 10:31:51 +0000

    runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Mutiple Coroutines
+"""
+from asyncio import run
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Multiple Coroutines"""
+    start = time.time()
+
+    run(wait_n(n, max_delay))
+
+    end = time.time()
+
+    exec_time = end - start
+
+    return exec_time / n

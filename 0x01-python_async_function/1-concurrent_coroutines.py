2383a9181e71fb394efbe6bdbed21df75bc5e6ac
Wmugacha <wilfredmugacha@gmail.com>
2023-08-07 09:43:23 +0000

    run concurrently

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module to run Wait_random n times
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    """Async function to run wait_random n times"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+
+    results = await asyncio.gather(*tasks)
+
+    return sorted(results)

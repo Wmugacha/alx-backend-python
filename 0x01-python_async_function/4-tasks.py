94567c86dd9aa64680157326cc10ba693f52fd0d
Wmugacha <wilfredmugacha@gmail.com>
2023-08-07 14:16:54 +0000

    reconfigure function

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module to run Wait_random n times
+"""
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Async function to run wait_random n times"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    results = await asyncio.gather(*tasks)
+
+    return sorted(results)

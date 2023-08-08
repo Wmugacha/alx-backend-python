a1bd8727f5edd123858009f12a450611a85ca1db
Wmugacha <wilfredmugacha@gmail.com>
2023-08-08 18:13:46 +0000

    async loop

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """Return a list of 10 random numbers"""
 
     result = [i async for i in async_generator()]
 

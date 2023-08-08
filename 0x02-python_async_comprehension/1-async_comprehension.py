2ff7050c41a755f0bcf8e67d6e6c08fa6a1133e6
Wmugacha <wilfredmugacha@gmail.com>
2023-08-08 18:10:38 +0000

    async loop

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,8 +8,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    result = []
-    async for i in async_generator():
-        result.append(i)
 
-    return(result)
+    result = [i async for i in async_generator()]
+    
+    return result

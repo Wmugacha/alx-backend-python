c1341cf675332829f8d58ba7252b294606263bc1
Wmugacha <wilfredmugacha@gmail.com>
2025-05-18 17:58:36 +0800

    process batch

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -11,12 +11,21 @@ def connect_db():
     print("Connected Successfully!!")
     return conn
 
-def stream_users():
+def stream_users_in_batches(batch_size):
     conn = connect_db()
     cursor = conn.cursor()
 
     query = "SELECT user_id, name, email, age FROM user_data"
     cursor.execute(query)
 
-    for row in cursor:
-        yield row
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        filtered_batch = [row for row in batch if row[3] > 25]
+        for row in filtered_batch:
+            yield row
--- python-generators-0x00/1-main.py
+++ python-generators-0x00/1-main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 from itertools import islice
-stream_users = __import__('0-stream_users')
+import sys
 
-# iterate over the generator function and print only the first 6 rows
+# Import the module
+stream_users_module = __import__('0-stream_users')
 
+# Get the generator function from the module
+stream_users = stream_users_module.stream_users
+
+# Use islice to fetch first 6 rows
 for user in islice(stream_users(), 6):
     print(user)

30ed11791a3653746158859d8ac8c517b8c55a39
Wmugacha <wilfredmugacha@gmail.com>
2025-05-21 21:47:28 +0800

    retry function

---
+++ python-decorators-0x01/4-cache_query.py
@@ -0,0 +1,30 @@
+import time
+import sqlite3 
+import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            return func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+    return wrapper
+
+query_cache = {}
+
+"""your code goes here"""
+
+@with_db_connection
+@cache_query
+def fetch_users_with_cache(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+#### First call will cache the result
+users = fetch_users_with_cache(query="SELECT * FROM users")
+
+#### Second call will use the cached result
+users_again = fetch_users_with_cache(query="SELECT * FROM users")

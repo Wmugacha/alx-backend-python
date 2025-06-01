1ef7455af683bad9a58c5083b1894b3fec69bd32
Wmugacha <wilfredmugacha@gmail.com>
2025-06-01 08:05:48 +0800

    create conversation & new message

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import CustomUser, Message, Conversation
 from .serializers import UserSerializer, MessageSerializer, ConversationSerializer
 
@@ -11,6 +13,37 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
+    def create(self, request, *args, **kwargs):
+        conversation_id = request.data.get("conversation")
+        message_body = request.data.get("message_body")
+
+        if not conversation_id or not message_body:
+            return Response({"error": "conversation and message_body are required"}, status=status.HTTP_400_BAD_REQUEST)
+            
+        conversation = get_object_or_404(Conversation, conversation_id=conversation_id)
+        sender = request.user
+
+        message = Message.objects.create(
+            conversation=conversation,
+            sender=sender,
+            message_body=message_body
+        )
+
+        serializer = self.get_serializer(message)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        user_ids = request.data.get("participants")
+        if not user_ids or not isinstance(user_ids, list):
+            return Response({"error": "participants must be a list of user_ids"}, status=status.HTTP_400_BAD_REQUEST)
+
+        conversation= Conversation.objects.create()
+        conversation.participants.set(user_ids)
+        conversation.save()
+
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

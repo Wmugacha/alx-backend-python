feaed6a9620f62e79dfbb82c62664e46f4aea808
Wmugacha <wilfredmugacha@gmail.com>
2025-06-06 14:58:28 +0800

    jwt permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,10 +1,10 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsMessageOwner(BasePermission):
+class IsMessageOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.sender == request.user
 
 
-class ConversationOwner(BasePermission):
+class ConversationOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user in obj.participants.all()

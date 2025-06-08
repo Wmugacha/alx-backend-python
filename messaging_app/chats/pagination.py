1bed05fd605b6aa0187583141480aa76e1549e6f
Wmugacha <wilfredmugacha@gmail.com>
2025-06-08 17:40:47 +0800

    pagination settings

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -5,7 +5,7 @@ class MessagePagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-    def get_paginated_messages(self, data):
+    def get_paginated_response(self, data):
         return response.Response({
             'count': self.page.paginator.count,
             'next': self.get_next_link(),

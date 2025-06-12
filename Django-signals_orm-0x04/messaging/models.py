627861f5493b24d446d1d2dfe334945ecffcb8d5
Wmugacha <wilfredmugacha@gmail.com>
2025-06-12 18:30:51 +0800

    django signals model

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Message:
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)

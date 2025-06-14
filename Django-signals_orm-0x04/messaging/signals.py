6642c576d1aa069ffe5b0921666491e7d73d31ff
Wmugacha <wilfredmugacha@gmail.com>
2025-06-14 21:14:47 +0800

    signal to delete user data

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,7 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
@@ -19,3 +21,9 @@ def edit_message(sender, instance, **kwargs):
                 edited_by = instance.sender
             )
             instance.edited = True
+
+@receiver(post_delete, sender=User)
+def user_data_cleanup(sender, instance, **kwargs):
+    Message.objects.filter(Q(sender=instance) | Q(receiver=instance)).delete()
+    MessageHistory.objects.filter(edited_by=instance).delete()
+    Notification.objects.filter(Q(recipient=instance) | Q(sender=instance)).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
+from .models import Message, Notification, MessageHistory
+from .serializers import UserSerializer, MessageSerializer, NotificationSerializer, MessageHistorySerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=True, method=['delete'], url_path='custom-delete')
+    def delete_user(self, request, pk=None):
+        user = self.get_object()
+        user.delete()
+        return Response({"message": "User deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+    return Message.objects.filter(sender=self.request.user)
+
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+    return Notification.objects.filter(recipient=self.request.user)
+
+
+class MessageHistoryViewSet(viewsets.ModelViewSet):
+    queryset = MessageHistory.objects.all()
+    serializer_class = MessageHistorySerializer
